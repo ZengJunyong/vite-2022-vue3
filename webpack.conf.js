924d676ee217f89d53438ddfa85612391e2dfb9f
junyong <junyong.zeng@rebilly.com>
2022-06-21 16:52:33 +0800

    Need to config webpack.conf.js manually in Webstorm

---
+++ webpack.conf.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/Users/junyong/learn/vite-2022-vue3/src'),
+        }
+    },
+};
